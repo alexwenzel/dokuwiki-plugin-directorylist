2adc84ecfbb9ebea4d13a01f1cb701d58ad0a3b0
Alexander Wenzel <alexander.wenzel@scim.de>
2014-02-27 19:54:44 +0100

    removed unused scripts

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
-/* DOKUWIKI:include js/jquery.tinysort.min.js */
-
 jQuery(function()
 {
-	jQuery('ul.directorylist li').tsort('a', {
-		order:'asc'
-	});
-
-	jQuery('ul.directorylist li.menu').on('click', function()
-	{
-		jQuery('ul.directorylist li').tsort('a', {
-			order:'desc'
-		});
-
-		console.log('click');
-	});
+	
 });
